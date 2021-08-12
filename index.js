0e2b70b87551a1212c8bb9d0caa1d6ee0f4f2a7b
komal <joshikomal252@gmail.com>
2021-08-12 11:54:57 +0530

    cors added

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const http = require('http');
 const app = require('./app');
 
-
+const cors = require('cors');
+const corsOptions ={
+    
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 const normalizePort = val => {
   const port = parseInt(val, 10);
 
